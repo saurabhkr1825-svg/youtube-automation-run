acfcecca902650766348cac69fda331abddc1725
Saurabh <saurabhkr1825@gmail.com>
2026-07-08 09:23:09 +0530

    Add Rockvix channel configuration and update metadata

--- test_youtube_auth.py
+++ test_youtube_auth.py
@@ -2,7 +2,7 @@ from app.youtube import YouTubeAuth
 from app.config import TOKENS_DIR
 
 auth = YouTubeAuth(
-    TOKENS_DIR / "channel8.json"
+    TOKENS_DIR / "channel9.json"
 )
 
 credentials = auth.authenticate()
